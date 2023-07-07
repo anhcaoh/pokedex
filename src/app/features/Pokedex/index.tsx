0da4a77ac9cee2d1e187e7b91886a3514b82ce98
Anh Cao <anh.cao@perchinsights.com>
2023-07-06 20:16:28 -0400

    feat(MVP): :sparkles: initial setup with next app

---
+++ src/app/features/Pokedex/index.tsx
@@ -0,0 +1,9 @@
+const Pokedex = () => {
+  return (
+    <div className="container m-auto w-1/2">
+      <h2>Pokedex</h2>
+      <input className="p-4 m-2" autoFocus />
+    </div>
+  );
+};
+export default Pokedex;
